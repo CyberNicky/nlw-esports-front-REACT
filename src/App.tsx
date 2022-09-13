c2178b5ee04323ac782177999e3662e2756d2e33
CyberNicky <nickyalbuquerque2008@gmail.com>
2022-09-13 18:07:46 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
 // JSX: Javascript + XML
+import './styles/main.css';
+import logoImg from './assets/Logo.svg';
+
+
 function App() {
-  return <h1>Olá, mundo</h1>
+  return (
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logoImg} alt="" />
+
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span> está aqui.
+      </h1>
+    </div>
+
+  )
 }
 
 export default App
+ 
